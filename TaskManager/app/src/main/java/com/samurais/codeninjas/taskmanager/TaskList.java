32086031022d7e85ef820c01c5f9e4ceea51ebb4
delizoderek <delizoderek@gmail.com>
2017-02-18 19:37:39 -0800

    Update TaskList

--- TaskManager/app/src/main/java/com/samurais/codeninjas/taskmanager/TaskList.java
+++ TaskManager/app/src/main/java/com/samurais/codeninjas/taskmanager/TaskList.java
@@ -5,20 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class TaskList extends Activity {
 
+    LinearLayout taskList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_list);
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //taskList = (LinearLayout)findViewById(R.id.list);
 
     }
 
     public void addTask(View v){
-        TextView txt = (TextView) findViewById(R.id.txt1);
-        txt.setText(v.getId() + "stuff");
+        taskList.addView(v);
     }
 }
